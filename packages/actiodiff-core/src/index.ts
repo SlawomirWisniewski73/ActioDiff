441a349e1a2dc593eb8b107f314a78becf695340
Sławomir Wiśniewski <144655582+SlawomirWisniewski73@users.noreply.github.com>
2025-07-22 16:23:49 +0200

    Create index.ts

---
+++ packages/actiodiff-core/src/index.ts
@@ -0,0 +1,5 @@
+// Eksportujemy wszystkie nowo zdefiniowane typy
+export * from './types.js';
+
+// W przyszłości będziemy tu eksportować główne klasy, np. ActioDiff
+// export { ActioDiff } from './ActioDiff.js';
